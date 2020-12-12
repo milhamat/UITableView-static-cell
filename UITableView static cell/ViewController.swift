f128463351e45eb3c572fdb170f75af509ec83a8
MilhamAt <M477ashiddiq@gmail.com>
2020-12-12 15:45:51 +0700

    Initial Commit

---
+++ UITableView static cell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView static cell
+//
+//  Created by Muhammad Ilham Ashiddiq Tresnawan on 12/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
